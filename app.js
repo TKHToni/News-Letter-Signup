af1300fbaba46d11fe39a4c563866556a6ec617f
TKHToni <taikitoni.h@gmail.com>
2021-08-04 11:27:11 -0300

    Complete Project

---
+++ app.js
@@ -0,0 +1,67 @@
+require("dotenv").config()
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+const client = require("@mailchimp/mailchimp_marketing"); // you need to add dependency first. See tips.
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+client.setConfig({
+  apiKey: process.env.API_KEY,
+  server: "us1",
+});
+
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+  }
+
+  const run = async () => {
+    try {
+      const response = await client.lists.addListMember("16716c495b", {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName
+        }
+      });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      console.log(err);
+      console.log(err.status);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+
+  run();
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running");
+});
+
+
+//API Key 48aea5072edee63d4feec4a55721b48e-us1
+
+// list ID 16716c495b
